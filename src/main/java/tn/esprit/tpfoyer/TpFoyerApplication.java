a9b0c08f17f7854022f6799936cb175ae5d861ff
FertaniYoussef <fertaniyoussef23@gmail.com>
2024-11-05 10:50:08 +0100

    real final

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,16 +1,12 @@
 package tn.esprit.tpfoyer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableScheduling
 @EnableAspectJAutoProxy
 public class TpFoyerApplication {
